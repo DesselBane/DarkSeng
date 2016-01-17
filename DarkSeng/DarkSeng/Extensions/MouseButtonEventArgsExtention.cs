43a94efabf80861448701e57edfc1bf749efbac6
Dark Phantom <dessel.bane@gmx.de>
2016-01-17 17:32:09 +0100

    Added MouseButtonEventArgsExtensions Added functionality for detecting the things under your mouse

---
+++ DarkSeng/DarkSeng/Extensions/MouseButtonEventArgsExtention.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace DarkSeng.Extensions
+{
+    public static class MouseButtonEventArgsExtention
+    {
+        /// <summary>
+        /// Detects the Visual item that is under the mouse when it was clicked
+        /// </summary>
+        /// <param name="sender">The sender of this Event</param>
+        public static DependencyObject UnderlyingObject(this MouseButtonEventArgs e, UIElement sender)
+        {
+            return VisualTreeHelper.HitTest(sender, e.GetPosition(sender)).VisualHit;
+        }
+
+        /// <summary>
+        /// Detects the Visual item that is under the mouse when it was clicked and searches for its Visual Parent of Type T
+        /// </summary>
+        /// <typeparam name="T">The Type of the Parent</typeparam>
+        /// <param name="sender">The sender of this Event</param>
+        public static T UnderlyingObject<T>(this MouseButtonEventArgs e, UIElement sender) where T : UIElement
+        {
+            return e.UnderlyingObject(sender).GetParentOfType<T>();
+        }
+    }
+}
