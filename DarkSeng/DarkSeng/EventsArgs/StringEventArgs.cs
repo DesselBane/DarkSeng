7f27ceb84bd14d9ae678c42b31ebf12b01c7f7a9
Dark Phantom <dessel.bane@gmx.de>
2016-01-17 17:43:36 +0100

    Added StringEventArgs for passing along a string

---
+++ DarkSeng/DarkSeng/EventsArgs/StringEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkSeng.EventsArgs
+{
+    public class StringEventArgs : EventArgs
+    {
+        #region Properties
+
+        public string Message { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public StringEventArgs(string msg)
+        {
+            Message = msg;
+        }
+
+        #endregion
+    }
+}
