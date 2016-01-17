da3181962ae1a28acba272dcda0f81e6e73c74d8
Dark Phantom <dessel.bane@gmx.de>
2016-01-17 17:20:49 +0100

    Added DependencyObjectExtensions Added Functionality for getting a parent of type T from a DependencyObject removed unnecessary usings

---
+++ DarkSeng/DarkSeng/Extensions/DependencyObjectExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DarkSeng.Extensions
+{
+    public static class DependencyObjectExtensions
+    {
+        /// <summary>
+        /// Gets the visual parent of a certain type from a DependencyObject. e.g. You have a TabViewItem and want to have the TabView
+        /// </summary>
+        /// <typeparam name="T">Specifies the Type of the Parent e.g. TabView</typeparam>
+        /// <returns>Returns null if no Parent was of Type T otherwiese returns the Parent as T</returns>
+        public static T GetParentOfType<T>(this DependencyObject element) where T : DependencyObject
+        {
+            Type type = typeof(T);
+            if (element == null) return null;
+            DependencyObject parent = VisualTreeHelper.GetParent(element);
+            if (parent == null && ((FrameworkElement)element).Parent is DependencyObject) parent = ((FrameworkElement)element).Parent;
+            if (parent == null) return null;
+            else if (parent.GetType() == type || parent.GetType().IsSubclassOf(type)) return parent as T;
+            return GetParentOfType<T>(parent);
+        }
+    }
+}
