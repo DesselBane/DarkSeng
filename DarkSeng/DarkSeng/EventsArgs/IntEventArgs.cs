c120d0d44e7d5ebe7e4ad1020a0356c636962a11
Dark Phantom <dessel.bane@gmx.de>
2016-01-17 17:41:03 +0100

    Added IntEventArgs for passing along an int

---
+++ DarkSeng/DarkSeng/EventsArgs/IntEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DarkSeng.EventsArgs
+{
+    public class IntEventArgs : EventArgs
+    {
+        #region Properties
+
+        public int Value { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public IntEventArgs(int value)
+        {
+            Value = value;
+        }
+
+        #endregion
+    }
+}
