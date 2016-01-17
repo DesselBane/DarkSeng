8bbd03bbd8fc15616fbda52c2a413987e1e8ee6a
Dark Phantom <dessel.bane@gmx.de>
2016-01-17 17:37:31 +0100

    Added the Folder EventArgs in this folder there should be all EventArgs classes that are not too specific and could be used in more than one case Added BoolEventArgs for passing along a Boolean

---
+++ DarkSeng/DarkSeng/EventsArgs/BoolEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DarkSeng.EventsArgs
+{
+    public class BoolEventArgs : EventArgs
+    {
+        #region Properties
+
+        public bool Flag { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public BoolEventArgs(bool flag)
+        {
+            Flag = flag;
+        }
+
+        #endregion
+    }
+}
