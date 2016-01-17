2a2022bc6ab817420312700c68404bc4a1179baa
Dark Phantom <dessel.bane@gmx.de>
2016-01-17 17:40:07 +0100

    Added DateTimeEventArgs for passing along a DateTime

---
+++ DarkSeng/DarkSeng/EventsArgs/DateTimeEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DarkSeng.EventsArgs
+{
+    public class DateTimeEventArgs : EventArgs
+    {
+        #region Properties
+
+        public DateTime Date { get; private set; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        public DateTimeEventArgs(DateTime date)
+        {
+            Date = date;
+        }
+        
+        #endregion Constructors 
+    }
+}
