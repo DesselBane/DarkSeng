483a6d4750741c3245127cbd2317428665e3b124
Dark Phantom <dessel.bane@gmx.de>
2016-01-17 17:26:26 +0100

    Added ListExtensions Added functionality for coping a list to a new instance of a list with the same items inside

---
+++ DarkSeng/DarkSeng/Extensions/ListExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkSeng.Extensions
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Creates a new instance of this list and populates it with the instances of the old list
+        /// </summary>
+        /// <returns>A new List instance with the old items inside</returns>
+        public static List<object> DeepCopy(this IList listToClone)
+        {
+            var tmpList = new List<object>();
+
+            foreach (var itm in listToClone)
+            {
+                tmpList.Add(itm);
+            }
+
+            return tmpList;
+        }
+    }
+}
