f8426e8d9880a274f0da1965b8afb08123ae6915
Dark Phantom <dessel.bane@gmx.de>
2016-01-21 21:47:00 +0100

    Added Extensions for Converting a byte[] into a string

---
+++ DarkSeng/DarkSeng/Convert/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkSeng.Convert
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Converts a string into a bytearray
+        /// </summary>
+        public static byte[] ToByteArray(this string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        /// <summary>
+        /// Converts a bytearray of Unicode chars into a string
+        /// </summary>
+        public static string ToString(this byte[] bytes)
+        {
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+    }
+}
