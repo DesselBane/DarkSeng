f093e3ba114a46dbf7875c6adfa44e84238889a8
Stefan Engelhardt <engelhardt_stefan@gmx.de>
2016-02-02 16:36:53 +0100

    Added IEnumerableExtensions to the Extensions folder

---
+++ DarkSeng/DarkSeng/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DarkSeng.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        private static Random _rnd = new Random(Guid.NewGuid().GetHashCode());
+
+        /// <summary>
+        /// Shuffles a given collection and returns it as an IEnumerable
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="collection">Collection to shuffle</param>
+        /// <returns></returns>
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> collection)
+        {
+            int index = 0;
+
+            while (collection.Count() > 0)
+            {
+                index = _rnd.Next(0, collection.Count());
+                yield return collection.ElementAt(index);
+                collection = collection.Except(new[] { collection.ElementAt(index) });
+            }
+        }
+    }
+}
