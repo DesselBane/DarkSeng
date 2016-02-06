818619d54102b31270e6b096cf19008a2bd3747a
D4rthB4n3 <dessel.bane@gmx.de>
2016-02-06 17:59:16 +0100

    Created Commands Namespace for WPF Commands Created cmd_base where the basic events are implemented for convinience Created RelayCommand for easy use of commands

---
+++ DarkSeng/DarkSeng/Commands/RelayCommand.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkSeng.Commands
+{
+    public class RelayCommand : cmd_base
+    {
+        #region Vars
+        Action<object> _execute;
+        Func<object, bool> _canExecute;
+        #endregion Vars
+
+        #region Constructors
+        /// <summary>
+        /// Creates a new RelayCommand
+        /// </summary>
+        /// <param name="ExecuteDelegate">The delegate that will be called on with the Execute() Method</param>
+        /// <param name="CanExecuteDelegate">The delegate to determine if this command can be executed</param>
+        public RelayCommand(Action<object> ExecuteDelegate, Func<object, bool> CanExecuteDelegate)
+        {
+            if (ExecuteDelegate == null)
+                throw new ArgumentNullException("ExecutionDelegate must not be null.");
+
+            _execute = ExecuteDelegate;
+            _canExecute = CanExecuteDelegate;
+        }
+
+        /// <summary>
+        /// Creates a new RelayCommand
+        /// </summary>
+        /// <param name="ExecuteDelegate">The delegate that will be called on with the Execute() Method</param>
+        /// <param name="CanExecuteDelegate">The delegate to determine if this command can be executed</param>
+        public RelayCommand(Action ExecuteDelegate, Func<bool> CanExecuteDelegate) : this((object arg0) => ExecuteDelegate(), (object arg0) => CanExecuteDelegate()) { }
+        /// <summary>
+        /// Creates a new RelayCommand
+        /// </summary>
+        /// <param name="ExecuteDelegate">The delegate that will be called on with the Execute() Method</param>
+        public RelayCommand(Action ExecuteDelegate) : this(ExecuteDelegate, null as Func<object, bool>) { }
+        /// <summary>
+        /// Creates a new RelayCommand
+        /// </summary>
+        /// <param name="ExecuteDelegate">The delegate that will be called on with the Execute() Method</param>
+        public RelayCommand(Action<object> ExecuteDelegate) : this(ExecuteDelegate, null as Func<object, bool>) { }
+        /// <summary>
+        /// Creates a new RelayCommand
+        /// </summary>
+        /// <param name="ExecuteDelegate">The delegate that will be called on with the Execute() Method</param>
+        /// <param name="CanExecuteDelegate">The delegate to determine if this command can be executed</param>
+        public RelayCommand(Action ExecuteDelegate, Func<object, bool> CanExecuteDelegate) : this((object arg0) => ExecuteDelegate(), CanExecuteDelegate) { }
+        /// <summary>
+        /// Creates a new RelayCommand
+        /// </summary>
+        /// <param name="ExecuteDelegate">The delegate that will be called on with the Execute() Method</param>
+        /// <param name="CanExecuteDelegate">The delegate to determine if this command can be executed</param>
+        public RelayCommand(Action<object> ExecutionDelegate, Func<bool> CanExecuteDelegate) : this(ExecutionDelegate, (object arg0) => CanExecuteDelegate()) { }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// Determines if the command can be executed with the given parameter
+        /// </summary>
+        /// <param name="parameter">Some Parameter that might be needed for the command execution</param>
+        /// <returns>Returns true if command can be executed</returns>
+        public override bool CanExecute(object parameter)
+        {
+            if (_canExecute != null)
+                return _canExecute(parameter);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Executes the command with the given parameter
+        /// </summary>
+        /// <param name="parameter">Some Parameter that might be needed for the command execution</param>
+        public override void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        #endregion Methods
+    }
+}
---
+++ DarkSeng/DarkSeng/Commands/cmd_base.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DarkSeng.Commands
+{
+    public abstract class cmd_base : ICommand
+    {
+        #region Constructors
+
+        #endregion
+
+        #region Events
+        public event EventHandler CanExecuteChanged;
+
+        protected void OnCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+                handler(this, new EventArgs());
+        }
+        #endregion
+
+        #region Public
+
+        /// <summary>
+        /// Determines if the command can be executed with the given parameter
+        /// </summary>
+        /// <param name="parameter">Some Parameter that might be needed for the command execution</param>
+        /// <returns>Returns true if command can be executed</returns>
+        public abstract bool CanExecute(object parameter);
+
+        /// <summary>
+        /// Executes the command with the given parameter
+        /// </summary>
+        /// <param name="parameter">Some Parameter that might be needed for the command execution</param>
+        public abstract void Execute(object parameter);
+
+        /// <summary>
+        /// This will fire the CanExecuteChanged Event. This will also trigger the CommandManager to reevaluate this commands CanExecute State.
+        /// </summary>
+        public virtual void ReevaluateCanExecute()
+        {
+            OnCanExecuteChanged();
+        }
+
+        #endregion
+    }
+}
