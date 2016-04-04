57e6f80fb767b95189e74f12a299d281f4e64139
Seng3694 <engelhardt_stefan@gmx.de>
2016-04-04 10:25:26 +0200

    Shuffle Test

---
+++ DarkSeng/DarkSengUnitTests/IEnumerableExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DarkSeng.Extensions;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DarkSengUnitTests
+{
+    [TestClass]
+    public class IEnumerableExtensionTests
+    {
+        [TestMethod]
+        public void ShuffleTest()
+        {
+            List<int> orderedNumbers = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            List<int> unorderedNumbers = orderedNumbers.Shuffle().ToList();
+
+            Assert.AreNotEqual(orderedNumbers, unorderedNumbers);
+        }
+    }
+}
